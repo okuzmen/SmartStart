26867babf23b3f39299271b9c2f4269afa4be28d
Dima Oleshchenko <dima.oleschenko@gmail.com>
2013-07-12 12:42:38 +0300

    Add new hole: 1. UI updated. 2. Navigation fixed. 3. Controller refactoring.

--- Client/Web/app/js/app.js
+++ Client/Web/app/js/app.js
@@ -2,8 +2,29 @@
 
 // Declare app level module which depends on filters, and services
 var myApp = angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers']).
-  config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/Home', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-    $routeProvider.when('/AddNewHole', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-    $routeProvider.otherwise({redirectTo: '/Home'});
-  }]);
+    config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/Home', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
+        $routeProvider.when('/AddNewHole', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
+        $routeProvider.otherwise({redirectTo: '/Home'});
+    }]);
+
+myApp.run(function ($rootScope) {
+    $rootScope.$on('$routeChangeSuccess', function (ev, data) {
+        if (data.$$route && data.$$route.controller) {
+            var index;
+            var controllerName = data.$$route.controller.toString();
+            if (controllerName == "MyCtrl1") {
+                index = 0;
+            }
+            else if (controllerName == "MyCtrl2") {
+                index = 1;
+            }
+            else {
+                index = 0;
+            }
+
+            $rootScope.activeViewIndex = index;
+            console.log("controller changed: " + controllerName);
+        }
+    })
+});
--- Client/Web/app/js/controllers.js
+++ Client/Web/app/js/controllers.js
@@ -4,7 +4,7 @@
 
 angular.module('myApp.controllers', []).
     controller('MyCtrl1', function ($scope, DataService) {
-        $scope.statusMessage = "Data fetching..."
+        $scope.statusMessage = "Data fetching...";
         DataService.getAllHoles().then(function (data) {
             $scope.holes = data.results;
             $scope.$apply();
@@ -14,6 +14,50 @@ angular.module('myApp.controllers', []).
         }).fail(function (e) {
                 $scope.statusMessage = e.toString();
             });
+
+        function bindInfoWindow(marker, map, infowindow, html) {
+            google.maps.event.addListener(marker, 'click', function () {
+                infowindow.setContent(html);
+                infowindow.open(map, marker);
+            });
+        }
+
+        function markHollesOnMap(holes) {
+
+            var mapOptions = {
+                zoom: 8,
+                /*Dnipropetrovsk coordinates*/
+                center: new google.maps.LatLng(48.464764, 35.046193),
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            }
+
+            var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+            var infoWindow = new google.maps.InfoWindow({ content: '' });
+            var contentTemplate = loadContentTemplate("partials/MarkerInfoTemplate.html");
+
+            for (var index in holes) {
+                var hole = holes[index];
+                var latLng = new google.maps.LatLng(hole.location.latitude, hole.location.longitude);
+                var marker = new google.maps.Marker({
+                    position: latLng,
+                    title: hole.description
+                });
+                marker.setMap(map);
+                bindInfoWindow(marker, map, infoWindow, buildHtmlContent(hole.imagePath, hole.description, contentTemplate));
+            }
+        }
+
+        function buildHtmlContent(imagePath, description, template) {
+            return template.replace("{imgPath}", imagePath).replace("{descrText}", description);
+        }
+
+        function loadContentTemplate(filePath) {
+            //TODO: add validation check
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', filePath, false);
+            xhr.send(null);
+            return xhr.responseText;
+        }
     })
 
     .controller('MyCtrl2', function ($scope, DataService) {
@@ -23,6 +67,7 @@ angular.module('myApp.controllers', []).
             description: "",
             location: ""
         };
+
         var mapOptions = {
             zoom: 8,
             /*Dnipropetrovsk coordinates*/
@@ -37,10 +82,12 @@ angular.module('myApp.controllers', []).
             var lat = event.latLng.lat();
             var lng = event.latLng.lng();
             $scope.hole.location = {
-                latitude:lat,
-                longitude:lng
+                latitude: lat,
+                longitude: lng
             };
-            $scope.$apply();
+
+            setNewMarker(lat, lng);
+
         });
 
         $scope.createHole = function () {
@@ -57,50 +104,21 @@ angular.module('myApp.controllers', []).
         $scope.setFile = function (element) {
             alert(element);
         };
+
+        function setNewMarker(lat, lng) {
+            var latLng = new google.maps.LatLng(lat, lng);
+            var marker = new google.maps.Marker({
+                position: latLng,
+                title: ""
+            });
+            if ($scope.previousMarker != undefined) {
+                $scope.previousMarker.setMap(null);
+
+            }
+            $scope.previousMarker = marker;
+            marker.setMap(map);
+        }
     });
 
-function bindInfoWindow(marker, map, infowindow, html) {
-    google.maps.event.addListener(marker, 'click', function () {
-        infowindow.setContent(html);
-        infowindow.open(map, marker);
-    });
-}
-
-function markHollesOnMap(holes) {
-
-    var mapOptions = {
-        zoom: 8,
-        /*Dnipropetrovsk coordinates*/
-        center: new google.maps.LatLng(48.464764, 35.046193),
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    }
-
-    var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-    var infoWindow = new google.maps.InfoWindow({ content: '' });
-    var contentTemplate = loadContentTemplate("partials/MarkerInfoTemplate.html");
-
-    for (var index in holes) {
-        var hole = holes[index];
-        var latLng = new google.maps.LatLng(hole.location.latitude, hole.location.longitude);
-        var marker = new google.maps.Marker({
-            position: latLng,
-            title: hole.description
-        });
-        marker.setMap(map);
-        bindInfoWindow(marker, map, infoWindow, buildHtmlContent(hole.imagePath, hole.description, contentTemplate));
-    }
-}
-
-function buildHtmlContent(imagePath, description, template) {
-    return template.replace("{imgPath}", imagePath).replace("{descrText}", description);
-}
-
-function loadContentTemplate(filePath) {
-    //TODO: add validation check
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', filePath, false);
-    xhr.send(null);
-    return xhr.responseText;
-}
  
 
